a2f0d542497aa40376de760e24db17251f70a512
MikeHL29 <158159992+MikeHL29@users.noreply.github.com>
2024-08-10 15:13:46 +0700

    NvidiaTweakTool.bat

--- NvidiaTweakTool.bat
+++ NvidiaTweakTool.bat
@@ -1,22 +1,36 @@
 @echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges......
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 SETLOCAL EnableDelayedExpansion
 SET version=1.0
 title NvidiaTweakTool
 chcp 65000
 color a
 cls
-cls
->NUL 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" || (
-    echo.
-    echo Error
-    echo Please run as administrator
-    echo.
-    echo press any key to close
-    pause >NUL
-    exit /B
-)
-
-echo Running with administrator permissions.
 
 goto startmenu
 
